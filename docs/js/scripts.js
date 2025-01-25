a7098f9db5eed5836fc219f250517021393e2402
tarakichi <tarakichi48@gmail.com>
2025-01-25 17:13:16 +0900

    ID欄開放システムの作成

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,7 +1,7 @@
 //フォームの要素をチェックしてリダイレクトを行う関数
 function contentsCheck(userInput) {
     console.log(userInput);
-    if (userInput[3] == true) {
+    if(userInput[3] == true) {
         window.document.location.href = "AICRJMap6N.html";
     }
     else {
@@ -10,7 +10,9 @@ function contentsCheck(userInput) {
 }
 //進行状況に応じてフォームのdisableを解除
 function statusCheck() {
-    
+    if(localStorage.getItem("openPage1") == "true") {
+        document.getElementById("id").disabled = false;
+    }
 }
 //進行状況に応じてメッセージを出力
 function message() {
