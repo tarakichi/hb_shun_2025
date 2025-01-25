733fa4424437f2cb7a29978d8b428e84e4ac3bbb
tarakichi <tarakichi48@gmail.com>
2025-01-26 03:52:49 +0900

    page3までの作成

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,38 @@
 //フォームの要素をチェックしてリダイレクトを行う関数
 function contentsCheck(userInput) {
     console.log(userInput);
-    if(userInput[3] == true) {
-        window.document.location.href = "AICRJMap6N.html";
+    if(
+        userInput[0] == "" && 
+        userInput[1] == "" && 
+        userInput[2] == "" && 
+        userInput[3] == true &&
+        userInput[4] == "#000000" 
+    ) {
+        window.document.location.href = "AICRJMap6N.html"; //最初のidとpassword
+    } else if(
+        userInput[0] == "153132" && 
+        userInput[1] == "origin" && 
+        userInput[2] == "" && 
+        userInput[3] == false &&
+        userInput[4] == "#000000" 
+    ) {
+        window.document.location.href = "Nzcpw3lsWt.html"; //2つ目のidとpassword
+    } else if(
+        userInput[0] == "528952" && 
+        userInput[1] == "original" && 
+        userInput[2] == "" && 
+        userInput[3] == false &&
+        userInput[4] == "#000000" 
+    ) {
+        window.document.location.href = "jy1eK9YGST.html"; //
+    } else if(
+        userInput[0] == "1463" && 
+        userInput[1] == "humour" && 
+        userInput[2] == "Today is a fun day." && 
+        userInput[3] == false &&
+        userInput[4] == "#000000" 
+    ) {
+        window.document.location.href = "XqgNnZjSVB.html"; //
     }
     else {
         window.document.location.href = "blank.html";
@@ -13,6 +43,9 @@ function statusCheck() {
     if(localStorage.getItem("openPage1") == "true") {
         document.getElementById("id").disabled = false;
         document.getElementById("password").disabled = false;
+    }
+    if(localStorage.getItem("openPage3") == "true") {
+        document.getElementById("comment").disabled = false;
     }
 }
 //進行状況に応じてメッセージを出力
